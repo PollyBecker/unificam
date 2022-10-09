64957985b6824e84c92abf72169ff536ed602cc6
Polly Becker <pollyana.nbb@gmail.com>
2022-10-09 15:27:48 -0400

    correcao settings

--- Unificam/settings.py
+++ Unificam/settings.py
@@ -79,13 +79,10 @@ WSGI_APPLICATION = 'Unificam.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'basededados',
-        'USER': 'polly',
-        'PASSWORD':  "polly",
-        'HOST': "127.0.0.1",
-        'PORT': "5432",
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  "./db.sqlite3",
+
     }
 }
 
